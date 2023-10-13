754630da7d1e9dbab5802384754245918db469f8
Vithujan99 <vithujan99@hotmail.com>
2023-10-13 13:11:03 +0200

    changed to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTop from "./hooks/ScrollToTop";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
